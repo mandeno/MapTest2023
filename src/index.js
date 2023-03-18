9410b9b803bea677818671cd9f26fdf2d267cedc
Petronila Esther Mandeno <mandeno@digitalmappingsolutions.com>
2023-03-18 12:36:39 -0700

    Please work

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import custom from "./custom-style.json";
 let map;
 
 async function init() {
+    // line below should be repeated to add more data sources (along with all the other code in custom-style.json)
+    // const sites = await import("../data/sites.json")
+    const neighborhoods = await import("../data/output.json")
     const style = map.getStyle();
 
     style.sources = {
@@ -15,6 +18,9 @@ async function init() {
     };
     style.layers.push(...custom.layers);
     map.setStyle(style);
+    // This one below should also be repeated to add more data sources
+    // map.getSource("sites").setData(sites);
+    map.getSource("neighborhoods").setData(neighborhoods)
 }
 
 mapboxgl.accessToken = settings.accessToken;
